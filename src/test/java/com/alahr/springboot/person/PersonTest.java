dc9c739da498ec1d05650122c112e493836e9a64
alahr <eyibinghe@sina.com>
2018-08-29 16:13:57 +0800

    add junit4 test

---
+++ src/test/java/com/alahr/springboot/person/PersonTest.java
@@ -0,0 +1,41 @@
+package com.alahr.springboot.person;
+
+import com.alahr.springboot.person.domain.Person;
+import com.alahr.springboot.person.service.PersonService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigInteger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PersonTest {
+    private Logger logger = LoggerFactory.getLogger(PersonTest.class);
+
+    @Autowired
+    private PersonService personService;
+
+    @Test
+    public void test(){
+        BigInteger id = BigInteger.valueOf(1L);
+        Person person = personService.getById(id);
+        logger.info("Person name={}, gender={}", person.getName(), person.getGender());
+    }
+
+    @Before
+    public void before(){
+        logger.info("------------before--------------");
+    }
+
+    @After
+    public void after(){
+        logger.info("------------after--------------");
+    }
+}
